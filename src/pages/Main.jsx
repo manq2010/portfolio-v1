235c88ed1ad5fd0e94fc81d1cf818f90a14bf900
manq2010 <manq2010@gmail.com>
2023-01-17 03:30:06 +0200

    Add routing

---
+++ src/pages/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => (
+  <div>
+    <h1>test</h1>
+  </div>
+);
+
+export default Main;
---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Main from '../pages/Main';
+
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Main />} />
+  </Routes>
+);
+
+export default AppRoutes;
