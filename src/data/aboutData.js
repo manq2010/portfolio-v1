46c50fee92cbfba546a6d6c591b244065d0121d0
Mancoba <manq2010@gmail.com>
2023-04-26 11:04:36 +0200

    Update experience

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 const aboutData = {
-  description1: '💻 My name is Mancoba, I’m a software engineer currently pursuing coursework at Microverse, a remote software development program that uses pair programming and project building to teach development.',
-  description2: 'For more than four years, I was an IT Business Services Specialist. Each day, I worked with vendors from different cultures. So even before I started studying as a developer, I was already good at communication and a pro at working under immense pressure which is some of the skills needed to thrive in software development.',
-  description3: 'I have spent the last couple of months building websites such as landing pages, conference pages, SPAs, and a Rails API. I also learned how to work collaboratively with my peers and I know how to work on tight deadlines because I submitted projects every week and ensure I do not exceed the deadline. Also, I know how to learn new languages quickly!',
+  description1: '💻 My name is Mancoba and I am a full-stack software engineer with more than two years of experience in web development. In my role as a Full-Stack Developer, I possess extensive expertise in various programming languages such as Ruby, C++, and JavaScript, as well as multiple web development frameworks, including Ruby on Rails and React.js.',
+  description2: 'Prior to becoming a developer, I worked as an IT Business Services Specialist for over four years. My previous job involved collaborating with vendors from diverse cultures, which helped me sharpen my communication skills and excel under pressure.',
+  description3: 'Over the past year, I have been building cutting-edge applications, including landing pages, conference pages, SPAs, and Rails APIs. I have also managed projects using technologies such as Scrum, working collaboratively with colleagues to deliver high-quality work within tight deadlines. Additionally, I possess the ability to learn new programming languages quickly.',
   image: 'https://avatars.githubusercontent.com/u/53082877?s=600&u=8432a4860f75013af57e50bbbd974a89e51e94c9&v=4',
   stack: [{
     id: 1,
